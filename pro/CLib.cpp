b22b2fd015066c3376b19fd7a9ad57d04c9bfcdd
Le0AD <springisover000@gmail.com>
2016-07-08 22:31:55 +0300

    fn lib

--- pro/CLib.cpp
+++ pro/CLib.cpp
@@ -11,16 +11,65 @@ void initialize(CStash* s, int sz)
 	s->quantity = 0;
 	s->storage = 0;
 	s->next = 0; 
-
-
 }
 
-void cleanup(CStash* s);
+void cleanup(CStash* s)
+{
+	if (s->storage != 0)
+	{
+		cout << "freing storage" << endl;
+		delete[] s->storage;
+	}
+}
 
-int add(CStash* s, const void* element);
+int add(CStash* s, const void* element)
+{
+	if ((s->next) >= (s->quantity))
+	{
+		inflate(s, increment);
+	}
+	int start_bytes = s->next *s->size;
+	unsigned char* e = (unsigned char*)element;
+	for (int i = 0; i < s->size; i++)
+	{
+		s->storage[start_bytes + i] = e[i];
+	}
+	s->next++;
+	return(s->next, 1);
+}
 
-void* fetch(CStash* s, int index);
+void* fetch(CStash* s, int index)
+{
+	assert(0 <= index);
+	if (index >= s->next)
+	{
+		return 0;
+	}
+	return &(s->storage[index*s->size]);
+}
 
-int count(CStash* s);
+int count(CStash* s)
+{
+	return s->next;
+}
 
-void inflate(CStash* s, int increase);
+void inflate(CStash* s, int increase)
+{
+	assert(increase > 0);
+	int new_quantity = s->quantity + increase;
+	int new_bytes = new_quantity * s->size;
+	int old_bytes = s->quantity * s->size;
+	unsigned char* b = new unsigned char[new_bytes];
+	for (int i = 0; i < old_bytes; i++)
+	{
+		b[i] = s->storage[i];
+	}
+	delete[](s->storage);
+	s->storage = b;
+	s->quantity = new_quantity;
+	cout << "new_quantity:" << new_quantity
+		<< " new_bytes:" << new_bytes
+		<< " old_bytes:" << old_bytes
+		<< " s->size:" << s->size
+		<< endl;
+}
--- pro/main.cpp
+++ pro/main.cpp
@@ -1,31 +1,41 @@
+#include "CLib.h"
 #include <iostream>
 #include <string> 
 #include <fstream>
 #include <vector>
-
+#include <cassert>
 using namespace std;
 
-int *func(int c)
-{
-	cout << "fn" << endl;
-	return new int(31);
-}
-void func2()
-{
-
-}
-int **func3(int)
-{
-
-}
 int main()
 {
-	int (*(*pf1)(int));// int *func(int c)
-	void (*pf2)();// void func2()
-	int(*(*pf3)(int))[10];
-	pf3 = &func3;
-
-
+	CStash int_stash, string_stash;
+	int i;
+	char* cp;
+	ifstream in;
+	string line;
+	const int bufsize = 80;
+	initialize(&int_stash, sizeof(int));
+	for (i = 0; i < 100; i++)
+	{
+		add(&int_stash, &i);
+	}
+	for (i = 0; i < count(&int_stash); i++)
+	{
+		cout << "fetch(&int_stash, " << i << ")="
+			<< *(int*)fetch(&int_stash, i)
+			<< endl;
+	}
+	initialize(&string_stash, sizeof(char)*bufsize);
+	in.open("main.cpp");
+	assert(in);
+	while (getline(in, line))
+		add(&string_stash, line.c_str());
+	i = 0;
+	while ((cp = (char*)fetch(&string_stash, i++)) != 0)
+		cout << "fetch(&string_stash, " << i << ")="
+		<< cp << endl;
+	cleanup(&int_stash);
+	cleanup(&string_stash);
 	cin.get(); cin.get();
 	return 0;
 }
